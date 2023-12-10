b63ea2e6b29d1367988b7bcbbc3201cc922e8c23
archanakaklij <153357755+archanakaklij@users.noreply.github.com>
2023-12-10 15:26:03 +0530

    Update python_task_2.py

--- templates/python_task_2.py
+++ templates/python_task_2.py
@@ -12,8 +12,26 @@ def calculate_distance_matrix(df)->pd.DataFrame():
         pandas.DataFrame: Distance matrix
     """
     # Write your logic here
+    # Create a directed graph using networkx
+    G = nx.DiGraph()
 
-    return df
+    # Add edges with distances from the DataFrame
+    for _, row in df.iterrows():
+        G.add_edge(row['start_id'], row['end_id'], distance=row['distance'])
+        G.add_edge(row['end_id'], row['start_id'], distance=row['distance'])  # Add bidirectional edge
+
+    # Calculate the shortest path lengths between all pairs of nodes
+    shortest_path_lengths = dict(nx.all_pairs_dijkstra_path_length(G))
+
+    # Create a DataFrame with cumulative distances
+    distances_df = pd.DataFrame(shortest_path_lengths).fillna(0)
+
+    # Ensure the matrix is symmetric
+    distances_df = (distances_df + distances_df.T) / 2
+
+    return distances_df
+
+    
 
 
 def unroll_distance_matrix(df)->pd.DataFrame():
@@ -27,8 +45,24 @@ def unroll_distance_matrix(df)->pd.DataFrame():
         pandas.DataFrame: Unrolled DataFrame containing columns 'id_start', 'id_end', and 'distance'.
     """
     # Write your logic here
+    # Reset the index to get 'id_start' as a column
+    df_reset = df.reset_index()
 
-    return df
+    # Melt the DataFrame to transform it to the long format
+    melted_df = pd.melt(df_reset, id_vars='index', var_name='id_end', value_name='distance')
+
+    # Rename the columns
+    melted_df.columns = ['id_start', 'id_end', 'distance']
+
+    # Filter out rows where 'id_start' is equal to 'id_end'
+    melted_df = melted_df[melted_df['id_start'] != melted_df['id_end']]
+
+    # Sort the DataFrame by 'id_start' for consistency
+    melted_df = melted_df.sort_values(by=['id_start', 'id_end']).reset_index(drop=True)
+
+    return melted_df
+
+    
 
 
 def find_ids_within_ten_percentage_threshold(df, reference_id)->pd.DataFrame():
@@ -44,8 +78,21 @@ def find_ids_within_ten_percentage_threshold(df, reference_id)->pd.DataFrame():
                           of the reference ID's average distance.
     """
     # Write your logic here
+    # Filter rows where 'id_start' is the reference ID
+    reference_rows = df[df['id_start'] == reference_id]
 
-    return df
+    # Get the average distance of the reference ID
+    reference_avg_distance = reference_rows['distance'].mean()
+
+    # Calculate the threshold for 10% of the reference average distance
+    threshold = 0.1 * reference_avg_distance
+
+    # Filter rows where the average distance is within the threshold
+    filtered_df = df[(df['distance'] >= reference_avg_distance - threshold) &
+                     (df['distance'] <= reference_avg_distance + threshold)]
+
+    return filtered_df
+
 
 
 def calculate_toll_rate(df)->pd.DataFrame():
@@ -59,8 +106,17 @@ def calculate_toll_rate(df)->pd.DataFrame():
         pandas.DataFrame
     """
     # Wrie your logic here
+    # Assuming you have a function get_toll_rate(vehicle_type, distance) to get toll rates
+    def get_toll_rate(vehicle_type, distance):
+        # Your logic to determine toll rates based on vehicle type and distance
+        # Replace the following line with your actual calculation
+        return 0.1 * distance  # This is just a placeholder
+
+    # Apply the get_toll_rate function to each row
+    df['toll_rate'] = df.apply(lambda row: get_toll_rate(row['vehicle_type'], row['distance']), axis=1)
 
     return df
+    
 
 
 def calculate_time_based_toll_rates(df)->pd.DataFrame():
@@ -74,5 +130,13 @@ def calculate_time_based_toll_rates(df)->pd.DataFrame():
         pandas.DataFrame
     """
     # Write your logic here
+    # Assuming you have a function get_time_based_toll_rate(timestamp) to get toll rates
+def get_time_based_toll_rate(timestamp):
+        # Your logic to determine toll rates based on time
+        # Replace the following line with your actual calculation
+        return 0.1  # This is just a placeholder
+
+    # Apply the get_time_based_toll_rate function to each row
+    df['time_based_toll_rate'] = df['timestamp'].apply(get_time_based_toll_rate)
 
     return df
