a419ee78c696d1dfab593fb95920af33652a9aff
archanakaklij <153357755+archanakaklij@users.noreply.github.com>
2023-12-10 15:30:54 +0530

    Update python_task_1.py

--- templates/python_task_1.py
+++ templates/python_task_1.py
@@ -13,6 +13,10 @@ def generate_car_matrix(df)->pd.DataFrame:
                           where 'id_1' and 'id_2' are used as indices and columns respectively.
     """
     # Write your logic here
+    dataset_path = 'dataset-1.csv'
+result_matrix = generate_car_matrix(dataset_path)
+print(result_matrix)
+
 
     return df
 
@@ -28,6 +32,9 @@ def get_type_count(df)->dict:
         dict: A dictionary with car types as keys and their counts as values.
     """
     # Write your logic here
+    car_counts = df['car'].value_counts().to_dict()
+
+   
 
     return dict()
 
@@ -43,8 +50,13 @@ def get_bus_indexes(df)->list:
         list: List of indexes where 'bus' values exceed twice the mean.
     """
     # Write your logic here
+    bus_mean = df['bus'].mean()
 
+    # Identify indices where 'bus' values are greater than twice the mean
+    bus_indexes = df[df['bus'] > 2 * bus_mean].index.tolist()
     return list()
+
+    
 
 
 def filter_routes(df)->list:
@@ -58,6 +70,10 @@ def filter_routes(df)->list:
         list: List of route names with average 'truck' values greater than 7.
     """
     # Write your logic here
+    route_means = df.groupby('route')['truck'].mean()
+
+    # Filter routes with average 'truck' values greater than 7
+    filtered_routes = route_means[route_means > 7].index.tolist()
 
     return list()
 
@@ -73,10 +89,19 @@ def multiply_matrix(matrix)->pd.DataFrame:
         pandas.DataFrame: Modified matrix with values multiplied based on custom conditions.
     """
     # Write your logic here
+    # Write your logic here
+    def custom_multiply(value):
+        # Example condition: Multiply by 2 if value is greater than 5, else leave unchanged
+        return value * 2 if value > 5 else value
 
-    return matrix
+    # Apply the custom multiplication function to each element in the matrix
+    modified_matrix = matrix.applymap(custom_multiply)
 
+    return modified_matrix
 
+    
+
+   
 def time_check(df)->pd.Series:
     """
     Use shared dataset-2 to verify the completeness of the data by checking whether the timestamps for each unique (`id`, `id_2`) pair cover a full 24-hour and 7 days period
@@ -88,5 +113,29 @@ def time_check(df)->pd.Series:
         pd.Series: return a boolean series
     """
     # Write your logic here
+    # Convert the timestamp column to datetime
+    df['timestamp'] = pd.to_datetime(df['timestamp'])
+
+    # Group by unique (`id`, `id_2`) pairs
+    grouped_df = df.groupby(['id', 'id_2'])
+
+    # Define a function to check completeness for each group
+    def check_completeness(group):
+        # Check if the timestamp range covers a full 24-hour period
+        time_range_check = (group['timestamp'].max() - group['timestamp'].min()).total_seconds() >= 24 * 60 * 60
+
+        # Check if the timestamp range covers a full 7 days period
+        days_range_check = (group['timestamp'].max() - group['timestamp'].min()).days >= 7
+
+        # Return True if both conditions are met, else False
+        return time_range_check and days_range_check
+
+    # Apply the check_completeness function to each group and get a boolean series
+    completeness_series = grouped_df.apply(check_completeness)
+
+    return completeness_series
 
     return pd.Series()
+
+
+    
